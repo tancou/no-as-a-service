977b6a782e6e32a0cafd89a9a1f9cfcd2eb265e6
tancou <git.commits@tancou.me>
2025-07-29 21:23:01 +0200

    Try without cache

--- index.js
+++ index.js
@@ -57,6 +57,9 @@ app.get('/', (req, res) => {
   `;
 
   res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
   res.send(html);
 });
 
